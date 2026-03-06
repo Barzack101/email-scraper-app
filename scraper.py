0d8f208f75101589913ed79dfe99d8bccea79f5b
Barzack101 <carlodattanasio3@gmail.com>
2026-03-07 00:03:06 +0100

    Add subprocess call to install dependencies

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'openpyxl', 'pdfplumber', 'selenium', 'requests'])
 import os
 import json
 import requests
